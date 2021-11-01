deb85b89856a26ca6e1b97f2843ab6e33a0c426b
Shahrear Ahmed <shahrearahmedshuvon003@gmail.com>
2021-11-01 16:54:37 +0600

    12

--- src/Pages/Packages/Packages.js
+++ src/Pages/Packages/Packages.js
@@ -5,13 +5,13 @@ import "./Packages.css"
 const Packages = () => {
     const [events, setEvents] = useState([]);
     useEffect(() => {
-        fetch("")
+        fetch("https://raw.githubusercontent.com/ShahrearAhmed/travel-agency/master/public/package.json")
             .then((res) => res.json())
             .then((data) => setEvents(data));
     }, []);
     return (
         <div class="mt-5">
-            <h3>Upcoming Events</h3>
+            <h3>Package Tour</h3>
             <div className="package-container">
                 {events.map((event) => (
                     <Package event={event}></Package>
