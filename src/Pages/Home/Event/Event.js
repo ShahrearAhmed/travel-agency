8ebca720fa9accaca71bd48a469a361906b0eb4c
Shahrear Ahmed <shahrearahmedshuvon003@gmail.com>
2021-11-01 15:57:01 +0600

    ten

--- src/Pages/Home/Event/Event.js
+++ src/Pages/Home/Event/Event.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./Event.css";
 
 const Event = ({ event }) => {
-    const { img, name, description } = event;
+    const { img, name, description, price } = event;
     return (
         <div className="event">
             <img className="img-fluid" src={img} alt="" />
             <h3 className="p-2">{name}</h3>
             <p className="px-2">{description}</p>
+            <p className="price">Price : {price}</p>
         </div>
     );
 };
